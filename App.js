c932e73e12045a2b8a71d33b2aa5279387af9512
akpante3 <obije.victor5@gmail.com>
2020-01-20 23:43:00 +0100

    first commit - header

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import { Header } from './src/components/index';
+
+import {
+  // Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+
+
+export default App;
